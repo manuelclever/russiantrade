dd71d2b7f4c50d6a27b8f9eb0a92a5a86a1b7af6
manuel <manuel-clever@web.de>
2022-02-25 20:18:45 +0100

    initial database construction

--- src/main/java/eu/donttradewithrussia/database/datasource/database_installation.sh
+++ src/main/java/eu/donttradewithrussia/database/datasource/database_installation.sh
@@ -1,11 +1,11 @@
-mkdir /data/cinemadb &&
+mkdir /data/tradedb &&
 sudo mkdir /var/lib/pgsql &&
 sudo chown $USER:$USER pgsql
 sudo apt install postgresql-server &&
 sudo /usr/bin/postgresql-setup --initdb &&
 sudo chown -r postgres:postgres pgsql &&
-sudo mkdir /data/cinemadb &&
-sudo chown -r postgres:postgres /data/cinemadb &&
+sudo mkdir /data/tradedb &&
+sudo chown -r postgres:postgres /data/tradedb &&
 sudo chcon unconfined_u:object_r:postgresql_db_t:s0 /data &&
 sudo chcon unconfined_u:object_r:postgresql_db_t:s0 /data/cinemadb &&
 sudo nano /var/lib/pgsql/data/pg_hba.conf &&
