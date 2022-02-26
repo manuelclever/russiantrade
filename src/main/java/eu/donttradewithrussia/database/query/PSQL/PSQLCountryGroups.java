3f18dcd74d231d587e3c0a5890d2d16b406afc02
manuel <manuel-clever@web.de>
2022-02-26 16:16:46 +0100

    other

---
+++ src/main/java/eu/donttradewithrussia/database/query/PSQL/PSQLCountryGroups.java
@@ -0,0 +1,18 @@
+//package eu.donttradewithrussia.database.query.PSQL;
+//
+//public class PSQLCountryGroups {
+//    //join
+//
+//    //select
+//
+//    //update
+//
+//    //delete
+//
+//    //parameter
+//
+//    //queries
+//
+//}
+//}
+//
