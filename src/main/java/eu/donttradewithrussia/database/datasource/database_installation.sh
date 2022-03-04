d4f683f08f9a3122ca12c7ed04a1e50b8359a0ea
Manuel Clever <manuel-clever@web.de>
2022-03-04 19:49:37 +0100

    fixed error in directory spelling

--- src/main/java/eu/donttradewithrussia/database/datasource/database_installation.sh
+++ src/main/java/eu/donttradewithrussia/database/datasource/database_installation.sh
@@ -7,15 +7,15 @@ sudo /usr/bin/postgresql-setup --initdb &&
 #create database directory with correct rights
 sudo mkdir /data/trade_db &&
 sudo chown -R postgres:postgres /data/trade_db &&
-sudo mkdir /data/tradeTest_db &&
-sudo chown -R postgres:postgres /data/tradeTest_db &&
+sudo mkdir /data/tradetest_db &&
+sudo chown -R postgres:postgres /data/tradetest_db &&
 sudo chcon unconfined_u:object_r:postgresql_db_t:s0 /data &&
 sudo chcon unconfined_u:object_r:postgresql_db_t:s0 /data/tradedb &&
 sudo systemctl enable postgresql &&
 sudo systemctl start postgresql &&
 
 #set validation
-sudo -u postgres psql; &&
+sudo -u postgres psql &&
 ALTER ROLE postgres WITH PASSWORD '1551';
 exit;
 sudo nano /var/lib/pgsql/data/pg_hba.conf
