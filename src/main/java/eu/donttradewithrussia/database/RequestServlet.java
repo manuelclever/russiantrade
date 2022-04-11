400a4337dab0adfd1f565c1a0ef9991cb970adbb
Manuel Clever <manuel-clever@web.de>
2022-04-11 12:31:31 +0200

    barebone servlet

--- src/main/java/eu/donttradewithrussia/database/RequestServlet.java
+++ src/main/java/eu/donttradewithrussia/database/RequestServlet.java
@@ -1,14 +1,20 @@
 package eu.donttradewithrussia.database;
 
+import eu.donttradewithrussia.database.datasource.DSCreator;
+import eu.donttradewithrussia.database.readAndWrite.dataset.PSQLDatasetReader;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 public class RequestServlet extends HttpServlet {
-
+    private static final Path DB_PROPERTIES = FileSystems.getDefault().getPath(
+            "src", "test", "resources", "database", "testDatabase.properties");
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Set the response message's MIME type
@@ -17,6 +23,13 @@ public class RequestServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         try {
+            String country = req.getParameter("country");
+            String type = req.getParameter("type");
+            String commodity = req.getParameter("commodity");
+
+            DSCreator dsC = new DSCreator(DB_PROPERTIES);
+            PSQLDatasetReader dr = new PSQLDatasetReader(dsC.getDataSourceTradeDB());
+
             out.println("<!DOCTYPE html");  // HTML 5
             out.println("<html><head>");
             out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
