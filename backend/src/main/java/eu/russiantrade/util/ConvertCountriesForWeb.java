deea5a45b33a3619f41bf386d45b914857dbb380
manuel <manuel-clever@web.de>
2022-04-25 21:38:15 +0200

    reworked syntax

--- backend/src/main/java/eu/russiantrade/util/ConvertCountriesForWeb.java
+++ backend/src/main/java/eu/russiantrade/util/ConvertCountriesForWeb.java
@@ -8,8 +8,10 @@ import java.util.List;
 public class ConvertCountriesForWeb {
     private static final String SOURCE_PATH = FileSystems.getDefault()
             .getPath("backend", "src", "main", "resources", "countries.txt").toAbsolutePath().toString();
-    private static final String TARGET_PATH = FileSystems.getDefault()
+    private static final String TARGET_PATH_JSX = FileSystems.getDefault()
             .getPath("webapp", "webpage", "src", "resources", "data", "countriesDropdown.txt")
+            .toAbsolutePath().toString();private static final String TARGET_PATH_IMPORT = FileSystems.getDefault()
+            .getPath("webapp", "webpage", "src", "resources", "data", "countriesImportImg.txt")
             .toAbsolutePath().toString();
     public static final String IMAGE_PATH = FileSystems.getDefault()
             .getPath("webapp", "webpage", "src", "resources", "images", "svg_flags")
@@ -22,7 +24,6 @@ public class ConvertCountriesForWeb {
     public static void convert() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(SOURCE_PATH));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(TARGET_PATH));
 
             List<String[]> countries = new ArrayList<>();
             while(reader.ready()) {
@@ -33,24 +34,46 @@ public class ConvertCountriesForWeb {
             }
             countries.sort((o1, o2) -> o1[1].compareTo(o2[1]));
 
-            writer.write("const countryOptions: CountryOption[] = [\n");
-            for(int i = 0; i < countries.size(); i++) {
-                String[] country = countries.get(i);
-                writer.write(
-                        "\t{ value: '" + country[1] + "',label: (\n" +
-                            "\t\t<div className='label'>\n" +
-                                "\t\t\t<img src={" + country[2].toLowerCase() + "_flag} alt='" +
-                                    country[1].toLowerCase() + "_flag' style={style" + ".img}/>\n" +
-                                "\t\t\t<span>" + country[1] + "</span>\n" +
-                            "\t\t</div>\n" +
-                        "\t)},\n");
-            }
-            writer.write("];");
-            writer.close();
+            writeJSXSyntax(countries);
+            writeImportImageSyntax(countries);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private static void writeJSXSyntax(List<String[]> countries) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(TARGET_PATH_JSX));
+
+        writer.write("const countryOptions: CountryOption[] = [\n");
+        for(int i = 0; i < countries.size(); i++) {
+            String[] country = countries.get(i);
+            String fullName = country[1].toLowerCase().replace("\s", "_");
+            String iso = country[2].toLowerCase();
+            writer.write(
+                    "\t{ value: '" + fullName + "',label: (\n" +
+                            "\t\t<div className='label'>\n" +
+                            "\t\t\t<img src={" + iso + "_flag} alt='" +
+                                fullName + "_flag' style={style" + ".img}/>\n" +
+                            "\t\t\t<span>" + country[1] + "</span>\n" +
+                            "\t\t</div>\n" +
+                            "\t)},\n");
+        }
+        writer.write("];");
+        writer.close();
+    }
+
+    private static void writeImportImageSyntax(List<String[]> countries) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(TARGET_PATH_IMPORT));
+
+        for(int i = 0; i < countries.size(); i++) {
+            String[] country = countries.get(i);
+            String fullName = country[1].toLowerCase().replace("\s", "-");
+            String iso = country[2].toLowerCase();
+
+            writer.write("import " + iso + "_flag from './../../resources/images/svg_flags/" +
+                fullName + ".svg;\n");
+        }
+        writer.close();
+    }
 }
